97d970fc30106cc94f9648a934bcf0b05715d74e
Elion Abdyli <53652464+elion-abdyli@users.noreply.github.com>
2026-06-13 21:14:49 +0000

    add boilerplate time axis

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import duckdb
+import pandas as pd
+import seaborn as sns
+import plotly.express as px
+st.set_page_config(page_title='pagetitle', layout='wide', initial_sidebar_state=None, menu_items=None)
+
+st.write("welcome")
+
+rel = duckdb.sql("""
+                 create or replace sequence hsec;
+                 select
+                 range::date as r,
+                 1 as one,
+                 row_number () over (order by range) as hsec,
+                 1.01**hsec as exp2
+                 from range(date '2026-01-01', date '2027-01-01', interval 1 day)
+                 """)
+
+c1,c2 = st.columns(2)
+
+c1.dataframe(rel, height = 600)
+# st.data_editor(rel)
+
+c2.scatter_chart(rel.df().set_index('r'), size = 5, height = 600)
