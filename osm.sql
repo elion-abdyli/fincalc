9714b84e711ec2c4b4780330590226b673bb7168
Elion Abdyli <53652464+elion-abdyli@users.noreply.github.com>
2026-07-11 19:33:22 +0000

    osm

--- osm.sql
+++ osm.sql
@@ -1,9 +1,23 @@
 .echo on
 .timer on
 
-set force_download=true;
-CREATE OR REPLACE TABLE bronze AS SELECT * FROM read_json('https://overpass-api.de/api/interpreter?data=[out:json];area["ISO3166-2"="CA-QC"]->.searchArea;node["amenity"="pharmacy"](area.searchArea);out;');
-from bronze;
-select unnest(elements, recursive:=true) from bronze;
-describe select unnest(elements, recursive:=true) from bronze;
+INSTALL spatial; LOAD spatial;
 
+set force_download=true;
+
+CREATE OR REPLACE TABLE bronze AS
+SELECT * FROM read_json('https://overpass-api.de/api/interpreter?data=[out:json];area["ISO3166-2"="CA-QC"]->.searchArea;node["amenity"="pharmacy"](area.searchArea);out;');
+
+CREATE OR REPLACE TABLE silver_pharmacies AS
+WITH elems AS (
+    SELECT unnest(elements, recursive := true) FROM bronze
+)
+SELECT
+    id AS osm_id,
+    tags,
+    ST_SetCRS(ST_Point(lon, lat), 'OGC:CRS84') AS geom
+FROM elems
+WHERE lat IS NOT NULL AND lon IS NOT NULL;
+
+describe silver_pharmacies;
+from silver_pharmacies limit 5;
